a5edfafab33cc7bd3655f85871dc74d34a66045d
Dan Kiss and Lainey Rokkan <lainey@>
2017-09-25 13:46:23 -0700

    add in task edit erb

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
+require('sinatra/activerecord')
 require('sinatra')
 require('sinatra/reloader')
-require('sinatra/activerecord')
 require('./lib/task')
 require('./lib/list')
 also_reload('lib/**/*.rb')
@@ -10,6 +10,7 @@ require('pry')
 # DB = PG.connect({:dbname => "to_do_test"})
 
 get("/") do
+  @tasks = Task.all()
   erb(:index)
 end
 
@@ -52,5 +53,18 @@ delete("/lists/:id") do
   @list = List.find(params.fetch("id").to_i())
   @list.delete()
   @lists = List.all()
+  erb(:index)
+end
+
+get('/tasks/:id/edit') do
+  @task = Task.find(params.fetch("id").to_i())
+  erb(:task_edit)
+end
+
+patch("/tasks/:id") do
+  description = params.fetch("description")
+  @task = Task.find(params.fetch("id").to_i)
+  @task.update({:description => description})
+  @tasks = Task.all()
   erb(:index)
 end
