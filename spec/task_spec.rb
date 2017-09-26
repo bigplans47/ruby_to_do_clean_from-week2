9e5eea92937b223bd1e9778be23b544820db3cb2
Dan Kiss <flowfast47@gmail.com>
2017-09-26 05:35:10 -0700

    add passing spec for associating tasks with list

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -16,6 +16,14 @@ describe(Task) do
       expect(Task.not_done()).to(eq(not_done_tasks))
     end
   end
+
+  describe("#list") do
+    it("tells which list it belongs to") do
+      test_list = List.create({:name => "list"})
+      test_task = Task.create({:description => "task", :list_id => test_list.id})
+      expect(test_task.list()).to(eq(test_list))
+    end
+  end
   # describe(".all") do
   #   it("is empty at first") do
   #     expect(Task.all()).to(eq([]))
